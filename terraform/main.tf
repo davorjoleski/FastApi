45c1e476c820a928e115b5e4b8487ab19a1d96d5
Davor <davor.j@live.com>
2025-07-28 17:57:50 +0200

    Implemented Terraform With AKS and gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # 4. Attach ACR to AKS (needed manually)
 resource "azurerm_role_assignment" "acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.identity.principal_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.identity[0].principal_id
   role_definition_name            = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
 }
