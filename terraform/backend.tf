8dc3d2bbc055866797276131a946054e71764192
Davor <davor.j@live.com>
2025-07-29 11:59:04 +0200

    Implemented Terraform With AKS and gitignore1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tfstate"         # RG containing the state storage account
-    storage_account_name  = "tfstateacct"     # Name of the storage account for state
-    container_name        = "tfstate"         # Blob container for state
+    resource_group_name   = "terraform_boot"         # RG containing the state storage account
+    storage_account_name  = "terraformsstorage "     # Name of the storage account for state
+    container_name        = "intake"         # Blob container for state
     key                   = "terraform.tfstate"  # State file name
     use_azuread_auth      = true              # Authenticate using Azure AD (ARM_ env vars)
     # (ARM_TENANT_ID and other ARM_ vars will be provided by CI environment)
