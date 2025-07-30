aea42f7376e5cd571777572ac43d71f00d1cb476
Davor <davor.j@live.com>
2025-07-30 16:32:00 +0200

    Test12

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "fastapi-rg"
+    storage_account_name = "fastapistorageacct"
+    container_name       = "intake"
+    key                  = "terraform.tfstate"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "random_integer" "suffix" {
   max = 99999
 }
 resource "azurerm_container_registry" "acr" {
-  name                = "fasttapiacr${random_integer.suffix.result}"
+  name                = "fasttapiacr69418"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
