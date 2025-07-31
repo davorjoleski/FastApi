d0ca5f3e697b98578b63e9cfd47f041b721c763d
Davor <davor.j@live.com>
2025-07-31 13:17:39 +0200

    Reseting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "fastapi-rg"
+    resource_group_name   = "fastapi-rg"
     storage_account_name = "fastapistorageacct"
     container_name       = "intake"
     key                  = "terraform.tfstate"
