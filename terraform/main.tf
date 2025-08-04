f7d5a50c7c99ff2e90aace69b3db3776110b483c
Davor <davor.j@live.com>
2025-08-04 13:00:16 +0200

    Structured it

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.65.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+
 provider "azurerm" {
   features {}
 
