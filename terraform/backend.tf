af6d4f154ad3ab7bede3e291cc3766d4caf2206b
Davor <davor.j@live.com>
2025-08-08 15:13:21 +0200

    Main update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-# Defines remote backend for storing Terraform state (optional if local)
-# Uncomment and configure if using Azure Storage backend
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "fastapi-rg"
-    storage_account_name = "fastapistorageacct"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+# # Defines remote backend for storing Terraform state (optional if local)
+# # Uncomment and configure if using Azure Storage backend
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "fastapi-rg"
+#     storage_account_name = "fastapistorageacct"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
