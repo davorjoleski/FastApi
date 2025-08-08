480d7dc5205b5843887778011569e695aa94a679
Davor <davor.j@live.com>
2025-08-08 16:50:06 +0200

    fix yml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,23 @@ variable "location" {
   default = "West Europe"
 
 }
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
