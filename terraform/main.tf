19af0f1af778b621bd6dd3389bb4c99a735a4ce8
Davor <davor.j@live.com>
2025-08-08 15:18:53 +0200

    Main update

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,5 +97,5 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity.principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
