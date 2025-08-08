91742b53a7376dc31a43c81276566736e2143dcf
Davor <davor.j@live.com>
2025-08-08 17:10:03 +0200

    fix yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.fastapi_rg.name
   virtual_network_name = azurerm_virtual_network.fastapi_vnet.name
-  address_prefixes     = ["10.240.0.0/16"]
+  address_prefixes     = ["10.0.1.0/24"]
 
 
 }
